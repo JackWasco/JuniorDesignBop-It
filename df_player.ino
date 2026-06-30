5743203bbad57d0f2206e92b05498f557e9ef640
JackWasco <jfw35@pitt.edu>
2026-06-30 11:43:41 -0400

    correct df code

--- df_player.ino
+++ df_player.ino
@@ -7,29 +7,32 @@ DFRobotDFPlayerMini myDFPlayer;
 void setup() {
   pinMode(13, OUTPUT);
 
-  // Turn on immediately to confirm chip is running
+  // Confirm chip is running
   digitalWrite(13, HIGH);
-  delay(2000);
+  delay(500);
   digitalWrite(13, LOW);
+  delay(500);
 
   mySoftwareSerial.begin(9600);
-
-  if (!myDFPlayer.begin(mySoftwareSerial)) {
-    // fast blink = DFPlayer not found
-    while (true) {
-      digitalWrite(13, HIGH); delay(100);
-      digitalWrite(13, LOW);  delay(100);
-    }
-  }
-
-  // slow blink = DFPlayer found, attempting to play
-  digitalWrite(13, HIGH); delay(500);
-  digitalWrite(13, LOW);  delay(500);
+  myDFPlayer.begin(mySoftwareSerial);  // no check, just initialize
 
   myDFPlayer.volume(30);
   delay(500);
+
   myDFPlayer.play(1);
+  delay(2000);  // adjust to match length of track 1
+
+  myDFPlayer.play(2);
+  delay(2000);  // adjust to match length of track 2
+
+  myDFPlayer.play(3);
+  delay(2000);  // adjust to match length of track 3
 }
 
 void loop() {
+  // keep blinking continuously so you know the chip is alive
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(300);
 }
